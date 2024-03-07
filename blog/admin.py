25b6cc7fe2aa72693d3f4230859ac277efc4b46e
limvin <vinpage00@gmail.com>
2024-03-07 15:41:12 +0900

    블로그세팅

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+# tube > admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post, Comment, Tag
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Tag)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from .models import Post, Comment, Tag
+from .forms import CommentForm
 
-# Create your views here.
+
+def blog_list(request):
+    posts = Post.objects.all()
+    return render(request, "blog/blog_list.html", {"posts": posts})
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            author = request.user
+            message = form.cleaned_data["message"]
+            c = Comment.objects.create(author=author, message=message, post=post)
+            c.save()
+    return render(request, "blog/blog_detail.html", {"post": post, "form": form})
+
+
+def blog_create(request):
+    pass
+
+
+def blog_update(request, pk):
+    pass
+
+
+def blog_delete(request, pk):
+    pass
+
+
+def blog_tag(request, tag):
+    posts = Post.objects.filter(tags__name__iexact=tag)
+    # exact에 i가 붙으면 대소문자 구분하지 않음.
+    return render(request, "blog/blog_list.html", {"posts": posts})
